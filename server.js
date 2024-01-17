1aaf8cacf1fa78bde1c5f419d7458543e2c5f52b
Yashraj K <yashrajkanake2002@gmail.com>
2024-01-17 16:16:30 +0530

    task: TASK 8 : PUT API for slambook

--- server.js
+++ server.js
@@ -281,19 +281,27 @@ app.put('/slambook/:id', async (req, res) => {
   }
 });
 
-// app.delete('/slambook/:id', async (req, res) => {
-//   try {
-//     const deletedEntry = await SlamBook.findByIdAndDelete(req.params.id);
-//     if (deletedEntry) {
-//       res.json({ message: 'Entry Deleted' });
-//     } else {
-//       res.status(404).json({ message: 'Entry not found' });
-//     }
-//   } catch (error) {
-//     console.error('Error deleting slambook entry:', error);
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// });
+app.delete('/slambook/:id', async (req, res) => {
+    const entryId = req.params.id;
+
+  try {
+
+    const deletedEntry = await SlamBook.findByIdAndDelete(entryId);
+
+    if (deletedEntry) {
+
+      res.status(200).json({ message: 'Entry Deleted', entry: deletedEntry });
+    } else {
+
+      res.status(404).json({ message: 'Entry not found' });
+
+    }
+  } catch (error) {
+    console.error('Error deleting slambook entry:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
 // Start the server
 app.listen(PORT, HOSTNAME, () => {
   console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
